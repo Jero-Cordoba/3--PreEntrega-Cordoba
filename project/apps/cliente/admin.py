a0edaee4369a3e51ee45ca965c543a11d761a52d
Jero Córdoba <jerof.l.cordoba@gmail.com>
2023-07-16 18:40:40 -0300

    Noveno Commit

--- project/apps/cliente/admin.py
+++ project/apps/cliente/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.db import models
 
 # Register your models here.
-from .models import Cliente, Pais	
+from .models import Cliente, Pais
 
 admin.site.register(Cliente)
 admin.site.register(Pais)
