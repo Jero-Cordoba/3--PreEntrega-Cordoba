c3a67336048b2ff75a12115568ed0373d5085b84
Jero Córdoba <jerof.l.cordoba@gmail.com>
2023-07-27 19:16:33 -0300

    }}}

--- project/apps/cliente/admin.py
+++ project/apps/cliente/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.db import models
 
 # Register your models here.
-from .models import Cliente, Pais
+from .models import Cliente, Pais, Propiedad
 
 admin.site.register(Cliente)
 admin.site.register(Pais)
+admin.site.register(Propiedad)
